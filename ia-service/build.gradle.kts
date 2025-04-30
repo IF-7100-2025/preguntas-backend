00385a97dcda72f4247166feee4d58d9020abf0c
Brandon <brandonvargass12345@gmail.com>
2025-04-30 15:01:41 -0600

    feat(ia-service): estructura inicial del servicio

---
+++ ia-service/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "ucr.ac.cr.lerningcommunity"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2024.0.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.liquibase:liquibase-core")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ ia-service/src/main/java/ucr/ac/cr/lerningcommunity/iaservice/IaserviceApplication.java
@@ -0,0 +1,13 @@
+package ucr.ac.cr.lerningcommunity.iaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaserviceApplication.class, args);
+	}
+
+}
