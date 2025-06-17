c5ee3c62e198525c3e95b9f0a755cb67c2d51082
luisballar <luisballar123@gmail.com>
2025-06-17 12:17:25 -0600

    calificar quiz en progreso

--- question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/api/types/request/GradeToQuiz.java
+++ question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/api/types/request/GradeToQuiz.java
@@ -1,13 +1,17 @@
-package ucr.ac.cr.learningcommunity.questionservice.api.types.request;
-
-import ucr.ac.cr.learningcommunity.questionservice.jpa.entities.QuestionEntity;
-
-import java.util.List;
-
-public record GradeToQuiz(
-        List<QuestionEntity>
-
-
-) {
-
-}
+//package ucr.ac.cr.learningcommunity.questionservice.api.types.request;
+//
+//import ucr.ac.cr.learningcommunity.questionservice.jpa.entities.QuestionEntity;
+//
+//import java.time.LocalDateTime;
+//import java.util.List;
+//import java.util.UUID;
+//
+//public record GradeToQuiz(
+//        String userId,
+//        UUID quizId,
+//        LocalDateTime completed,
+//
+//
+//) {
+//
+//}
--- question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/handlers/commands/GradeToQuizHandler.java
+++ question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/handlers/commands/GradeToQuizHandler.java
@@ -4,8 +4,8 @@ import java.util.UUID;
 
 public interface GradeToQuizHandler {
     sealed interface Result {
-        record Succes(int status, String message) implements Result {}
+        record Success(int status, String message) implements Result {}
         record Error(int status, String message) implements Result {}
     }
-    Result SubmitQuiz(String userId, UUID quizId)
+    Result SubmitQuiz(String userId, UUID quizId);
 }
