9b0194c527a41101ddf366079b947844c8197607
Brandon <brandonvargass12345@gmail.com>
2025-06-18 23:05:45 -0600

    WIP: Guardando cambios temporales y actuales antes de mergear

---
+++ question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/api/types/request/ReportQuestionRequest.java
@@ -0,0 +1,7 @@
+package ucr.ac.cr.learningcommunity.questionservice.api.types.request;
+
+public record ReportQuestionRequest(String question_id,
+                                    String username,String reason,
+                                    String comment,
+                                    String reported_at)
+{}
---
+++ question-service/src/main/java/ucr/ac/cr/learningcommunity/questionservice/handlers/commands/impl/ReportQuestionHandlerImpl.java
@@ -0,0 +1,17 @@
+package ucr.ac.cr.learningcommunity.questionservice.handlers.commands.impl;
+
+import ucr.ac.cr.learningcommunity.questionservice.handlers.commands.ReportQuestionHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportQuestionHandlerImpl implements ReportQuestionHandler {
+
+
+    @Override
+    public ReportQuestionHandler.Result reportQuestion(ReportQuestionHandler.Command command) {
+        //TODO
+        return null;
+    }
+
+
+}
