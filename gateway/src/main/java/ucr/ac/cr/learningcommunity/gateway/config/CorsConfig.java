807a3916ca271563c60c98d7fdeaa000052f23dd
Brandon <brandonvargass12345@gmail.com>
2025-07-03 02:31:56 -0600

    cambio rutas

--- gateway/src/main/java/ucr/ac/cr/learningcommunity/gateway/config/CorsConfig.java
+++ gateway/src/main/java/ucr/ac/cr/learningcommunity/gateway/config/CorsConfig.java
@@ -6,13 +6,19 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:5173");
+        // Lista explícita de orígenes permitidos
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:5173",
+                "http://preguntas-frontend:5173",
+                "http://149.130.191.18:5173"
+        ));
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         config.setAllowCredentials(true);
